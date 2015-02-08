bf0b653f1a7e23fd055cd4c4f2f628f3af3404c8
worace <horace.d.williams@gmail.com>
2015-02-08 15:35:37 -0800

    add example implementation using 3 different mock libraries

--- spec/dwarf_spec.rb
+++ spec/dwarf_spec.rb
@@ -1,15 +1,28 @@
 require_relative "../lib/dwarf"
 require "rspec"
 
-Rspec.describe "Dwarf-Mountain interactions using Rspec mocks" do
+describe "Dwarf-Mountain interactions using Rspec mocks" do
   context "#mine" do
+    before do
+      @dwarf = Dwarf.new
+    end
+
     it "is excited when it finds gems" do
+      mountain = instance_double("Mountain", :mine => "gems")
+      @dwarf.mine(mountain)
+      expect(@dwarf.status).to eq("happy")
     end
 
     it "curses profusely when it finds coal" do
+      mountain = instance_double("Mountain", :mine => "coal")
+      @dwarf.mine(mountain)
+      expect(@dwarf.status).to eq("grumpy")
     end
 
     it "is confused when it finds something besides gems or coal" do
+      mountain = instance_double("Mountain", :mine => "malachite")
+      @dwarf.mine(mountain)
+      expect(@dwarf.status).to eq("confused")
     end
   end
 end
--- test/dwarf_test.rb
+++ test/dwarf_test.rb
@@ -4,23 +4,56 @@ require "minitest/spec"
 require "mocha/mini_test"
 
 describe "Dwarf-Mountain interactions using Minitest::Mock" do
+  before do
+    @dwarf = Dwarf.new
+  end
+
   it "is excited when it finds gems" do
+    mountain = Minitest::Mock.new
+    mountain.expect(:mine, "gems")
+    @dwarf.mine(mountain)
+    assert_equal "happy", @dwarf.status
+    mountain.verify
   end
 
   it "curses profusely when it finds coal" do
+    mountain = Minitest::Mock.new
+    mountain.expect(:mine, "coal")
+    @dwarf.mine(mountain)
+    assert_equal "grumpy", @dwarf.status
+    mountain.verify
   end
 
   it "is confused when it finds something besides gems or coal" do
+    mountain = Minitest::Mock.new
+    mountain.expect(:mine, "fool's gold")
+    @dwarf.mine(mountain)
+    assert_equal "confused", @dwarf.status
+    mountain.verify
   end
 end
 
 describe "Dwarf-Mountain interactions using Mocha" do
+  before do
+    @dwarf = Dwarf.new
+    @mountain = stub("Mountain")
+  end
+
   it "is excited when it finds gems" do
+    @mountain.expects(:mine).returns("gems")
+    @dwarf.mine(@mountain)
+    assert_equal "happy", @dwarf.status
   end
 
   it "curses profusely when it finds coal" do
+    @mountain.expects(:mine).returns("coal")
+    @dwarf.mine(@mountain)
+    assert_equal "grumpy", @dwarf.status
   end
 
   it "is confused when it finds something besides gems or coal" do
+    @mountain.expects(:mine).returns("pizza")
+    @dwarf.mine(@mountain)
+    assert_equal "confused", @dwarf.status
   end
 end
